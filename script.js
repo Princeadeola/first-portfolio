8f3b76206300cde624941c5f9649c65d858fde7b
A-Adeola <56828114+Princeadeola@users.noreply.github.com>
2021-09-28 11:04:20 -0700

    commited

---
+++ script.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+	$(window).scroll(function(){
+		if (this.scrollY > 20) {
+			$('.navbar').addClass('sticky');
+		} else {
+			$('.navbar').removeClass('sticky');
+		}
+		if (this.scrollY > 500) {
+			$('.scroll-up-btn').addClass('show');
+		}else{
+			$('.scroll-up-btn').removeClass('show');
+		}
+	});
+	// slide-up script
+	$('.scroll-up-btn').click(function() {
+		$('html').animate({scrollTop: 0});
+	});
+
+	// toggle menu/navbar script
+	$('.menu-bars').click(function(){
+		$('.navbar .menu').toggleClass("active");
+		$('.menu-bars i').toggleClass("active");
+	});
+
+	// typing animation script
+	var typed = new Typed(".typing", {
+		strings: ["Developer", "Designer", "Freelancer", "Tutor"],
+		typeSpeed: 100,
+		backSpeed: 60,
+		loop:true
+	});
+
+	var typed = new Typed(".typing-2", {
+		strings: ["Developer", "Designer", "Freelancer", "Tutor"],
+		typeSpeed: 100,
+		backSpeed: 60,
+		loop:true
+	});
+
+	// owl-carousel script
+	$('.carousel').owlCarousel({
+		margin: 23,
+		loop: true,
+		autoplayTimeOut: 2000,
+		autoplayHoverPause: true,
+		responsive: {
+			0:{
+				item: 1,
+				nav: false
+			},
+			500:{
+				item: 2,
+				nav: false
+			},
+			1000:{
+				item: 3,
+				nav: false
+			}
+		}
+	})	 
+
+});
